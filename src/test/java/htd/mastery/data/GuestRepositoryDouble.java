b30c6e2bddaf74845ee6c98bc690925962a49a5f
inkdmontana <tvazquez92@gmail.com>
2025-06-02 17:08:57 -0400

    added more test files

---
+++ src/test/java/htd/mastery/data/GuestRepositoryDouble.java
@@ -0,0 +1,4 @@
+package htd.mastery.data;
+
+public class GuestRepositoryDouble {
+}
---
+++ src/test/java/htd/mastery/data/HostFileRepositoryTest.java
@@ -0,0 +1,7 @@
+package htd.mastery.data;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HostFileRepositoryTest {
+
+}
